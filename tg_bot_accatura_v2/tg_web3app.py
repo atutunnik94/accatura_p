d9307a783fa70a9d8c226bbec0018632e475d223
atutunnik94 <atutunnik94@gmail.com>
2024-07-14 14:50:34 +0200

    1

--- tg_bot_accatura_v2/tg_web3app.py
+++ tg_bot_accatura_v2/tg_web3app.py
@@ -4,16 +4,15 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 
 class requestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        print('self')
+        print(self.path)
 
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
         self.wfile.write(("html_content + add_html_content").encode('utf-8'))
-        # return 're'
 
 if __name__ == '__main__':
-    print('Второй файл запущен')
+    print('tg_web3app.py: запущен')
     server_address = ('', 8080)
     server = HTTPServer(server_address, requestHandler)
     server.serve_forever()
